7d9ff26178c224f1a8fe68fb38b94e7374a925ed
myrellalinhares <107002111+myrellalinhares@users.noreply.github.com>
2024-09-25 11:05:45 -0300

    25/09

--- informacoeGlobais.js
+++ informacoeGlobais.js
@@ -8,6 +8,10 @@ async function visualizarInformacoesGlobais(params) {
     //contante que armazena uma repossta  await=espera ////fetch=faz a requisição
     const res = await fetch(url);
     const dados = await res.json();//espera as reposta erem convertidas em JSON
-console.log(dados);
+console.log(dados);//visualizar as imforações no console
+const paragrafo=document.createElement('p');//criar um parágrafo
+paragrafo.classList.add('graficos-container__texto');//adiciona uma clase do css ao parágrafo
+/*insere um texto "Você sabia que o mundo tem " +*/
+paragrafo.innerHTML =`Você sabia que o mundo tem ${dados.total_pessoas_mundo}`
 }
 visualizarInformacoesGlobais();//chama a função

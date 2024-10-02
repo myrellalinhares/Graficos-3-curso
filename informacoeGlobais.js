e393e22c0c797247e21ab5b974e2a4a4059975b0
myrellalinhares <107002111+myrellalinhares@users.noreply.github.com>
2024-10-02 11:01:50 -0300

    02/10

--- informacoeGlobais.js
+++ informacoeGlobais.js
@@ -8,16 +8,18 @@ async function visualizarInformacoesGlobais(params) {
     //contante que armazena uma repossta  await=espera ////fetch=faz a requisição
     const res = await fetch(url);
     const dados = await res.json();//espera as reposta erem convertidas em JSON
-console.log(dados);//visualizar as imforações no console
-const paragrafo=document.createElement('p');//criar um parágrafo
-paragrafo.classList.add('graficos-container__texto');//adiciona uma clase do css ao parágrafo
-/*insere um texto "Você sabia que o mundo tem " +*/
-paragrafo.innerHTML =`Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente ${dados.total_pessoas_conectadas}
-estão conectadas em alguma rede social e passam em média ${dados.tempo_medio}horas conectadas`//Insere o texto "Você sabia que o mundo tem"+total_pessoas_mundo                              
-const container=document.getElementById('grafico-container') //cria a variável "container",seleciona o ID(garficos-container) na section do HTML
+console.log(dados);//visualizar as informações no console
+const paragrafo=document.createElement('p');//criar um elemento de parágrafo
+
+paragrafo.classList.add('graficos-container__texto');//adiciona uma clase do CSS ao parágrafo
+
+/*insere o texto "Você sabia que o mundo tem "+total_pessoas_mundo ...*/
+paragrafo.innerHTML=`Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo}</span> de pessoas 
+e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma 
+rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+//cria a variável "container", seleciona o ID "graficos-container" na section do HTML                              
+const container= document.getElementById('graficos-container');
 container.appendChild(paragrafo);//Insere o paragrafo dentro do "container"
-
-
 
 }
 visualizarInformacoesGlobais();//chama a função

f6feab5fdf4c18b5201334395082d4c0a3ecf824
myrellalinhares <107002111+myrellalinhares@users.noreply.github.com>
2024-10-02 11:15:22 -0300

    02/10

--- informacoeGlobais.js
+++ informacoeGlobais.js
@@ -14,9 +14,9 @@ const paragrafo=document.createElement('p');//criar um elemento de parágrafo
 paragrafo.classList.add('graficos-container__texto');//adiciona uma clase do CSS ao parágrafo
 
 /*insere o texto "Você sabia que o mundo tem "+total_pessoas_mundo ...*/
-paragrafo.innerHTML=`Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo}</span> de pessoas 
-e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma 
-rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+paragrafo.innerHTML=`Você sabia que o mundo tem <span> ${pessoasNomundo}bilhões</span> de pessoas 
+e que aproximadamente <span>${pessoasConectadas}bilhões</span> estão conectadas em alguma 
+rede social e passam em média <span>${horas}</span> horas conectadas.`
 //cria a variável "container", seleciona o ID "graficos-container" na section do HTML                              
 const container= document.getElementById('graficos-container');
 container.appendChild(paragrafo);//Insere o paragrafo dentro do "container"

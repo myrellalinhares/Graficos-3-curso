a90873c9d7ae928118fad1635421fe566f34bd29
myrellalinhares <107002111+myrellalinhares@users.noreply.github.com>
2024-12-04 11:39:35 -0300

    04/12

--- common.js
+++ common.js
@@ -2,7 +2,13 @@ const getCSS=(vaiavel)=> {
 
     return getComputedStyle(document.body).getPropertyValue(variavel)
 }
-export {getCSS}
+const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+}
+
+export {getCSS, tickConfig }
 
 
 
